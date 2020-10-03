ea4c9e3da8ecfcacc46f44ea4550003dc97f9a46
Phil Dolanov <phildolganov@yahoo.com>
2020-10-03 16:28:50 -0700

    operators examples

---
+++ src/main/java/operators/CastingNumbers.java
@@ -0,0 +1,14 @@
+package operators;
+
+// What happens when you cast a float or double to an integral value?
+
+public class CastingNumbers {
+    public static void main(String[] args) {
+        double above = 0.7, below = 0.4;
+        float fabove = 0.7f, fbelow = 0.4f;
+        System.out.println("(int)above: " + (int) above);
+        System.out.println("(int)below: " + (int) below);
+        System.out.println("(int)fabove: " + (int) fabove);
+        System.out.println("(int)fbelow: " + (int) fbelow);
+    }
+}
---
+++ src/main/java/operators/Overflow.java
@@ -0,0 +1,10 @@
+package operators;
+
+public class Overflow {
+    public static void main(String[] args) {
+        int big = Integer.MAX_VALUE;
+        System.out.println("big = " + big);
+        int bigger = big * 4;
+        System.out.println("bigger = " + bigger);
+    }
+}
